fcee44409b18c9a8a4472483e9a8b3a1ad640767
Umair Usman <139118512+umair-666@users.noreply.github.com>
2023-11-26 15:42:28 +0530

    26th_Nov- Largest Submatrix With Rearrangements.cpp

---
+++ DSA questions/POTD/Leetcode/26th_Nov- Largest Submatrix With Rearrangements.cpp
@@ -0,0 +1,25 @@
+
+// 1727. Largest Submatrix With Rearrangements
+// https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/?envType=daily-question&envId=2023-11-26
+
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int ans = 0;
+        for (int row = 0; row<m; row++){
+            for(int col = 0; col<n; col++){
+                if(matrix[row][col] != 0 && row > 0){
+                    matrix[row][col] += matrix[row-1][col];
+                }
+            }
+            
+            vector<int> currRow = matrix[row];
+            sort(currRow.begin(), currRow.end(), greater());
+            for(int i=0; i<n;i++){
+                ans = max(ans, currRow[i] * (i+1));
+            }
+        }
+        return ans;
+    }
+};
