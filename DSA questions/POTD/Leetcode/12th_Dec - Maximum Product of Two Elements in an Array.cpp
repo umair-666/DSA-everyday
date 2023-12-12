459371ab10dec204982a2b262de70227953e5911
Umair Usman <139118512+umair-666@users.noreply.github.com>
2023-12-12 13:00:13 +0530

    Create 12th_Dec - Maximum Product of Two Elements in an Array.cpp

---
+++ DSA questions/POTD/Leetcode/12th_Dec - Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,15 @@
+// 1464. Maximum Product of Two Elements in an Array
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/?envType=daily-question&envId=2023-12-12
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int larger = nums[n-1];
+        int large = nums[n-2];
+        
+        return (larger-1) * (large-1);
+    }
+};
