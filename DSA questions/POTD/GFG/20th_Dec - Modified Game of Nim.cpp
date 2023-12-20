36678860ef1ec24e6b2248f0f54407277a63ac0e
Umair Usman <139118512+umair-666@users.noreply.github.com>
2023-12-20 12:06:41 +0530

    Create 20th_Dec - Modified Game of Nim.cpp

---
+++ DSA questions/POTD/GFG/20th_Dec - Modified Game of Nim.cpp
@@ -0,0 +1,16 @@
+// Modified Game of Nim
+// https://www.geeksforgeeks.org/problems/variation-in-nim-game4317/1
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        int xo = 0;
+        for(int i=0;i<n;i++)
+            xo ^= A[i];
+        
+        if(!xo) return 1;
+        else    return (n%2)+1;
+        
+        
+    }
+};
