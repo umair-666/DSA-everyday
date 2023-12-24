38950b3e1d39a5d81e8fbd411f82e4d4de5ab187
Umair Usman <139118512+umair-666@users.noreply.github.com>
2023-12-25 00:11:03 +0530

    Update and rename fkvf.cpp to 24th_Dec - Buy Maximum Stocks if i stocks can be bought on i-th day.cpp

--- DSA questions/POTD/GFG/24th_Dec - Buy Maximum Stocks if i stocks can be bought on i-th day.cpp
+++ DSA questions/POTD/GFG/24th_Dec - Buy Maximum Stocks if i stocks can be bought on i-th day.cpp
@@ -1,4 +1,11 @@
-  vector<pair<int,int>> a;
+// Buy Maximum Stocks if i stocks can be bought on i-th day
+// https://www.geeksforgeeks.org/problems/buy-maximum-stocks-if-i-stocks-can-be-bought-on-i-th-day/1
+
+class Solution {
+public:
+    int buyMaximumProducts(int n, int k, int price[]){
+        //Write your code here
+          vector<pair<int,int>> a;
         for(int i=0;i<n;i++){
             a.push_back({price[i],i+1});
         }
@@ -20,3 +27,5 @@
           
         }
         return count;
+    }
+};
