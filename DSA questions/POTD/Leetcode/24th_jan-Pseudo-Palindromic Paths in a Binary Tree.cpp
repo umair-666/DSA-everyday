151a1d74f64ced8e2eb32c48cac20dbeff246b15
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-01-24 10:55:53 +0530

    Create 24th_jan-Pseudo-Palindromic Paths in a Binary Tree.cpp

---
+++ DSA questions/POTD/Leetcode/24th_jan-Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -0,0 +1,35 @@
+//1457. Pseudo-Palindromic Paths in a Binary Tree
+//https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/?envType=daily-question&envId=2024-01-24
+
+class Solution {
+public:
+    int pseudoPalindromicPaths(TreeNode* root) {
+        int count = 0, path = 0;
+
+        stack<pair<TreeNode*, int>> stk;
+        stk.push({root, 0});
+
+        while (!stk.empty()) {
+            auto [node, path] = stk.top();
+            stk.pop();
+
+            if (node != nullptr) {
+                
+                path = path ^ (1 << node->val);
+
+                
+                if (node->left == nullptr && node->right == nullptr) {
+                    
+                    if ((path & (path - 1)) == 0) {
+                        ++count;
+                    }
+                } else {
+                    stk.push({node->right, path});
+                    stk.push({node->left, path});
+                }
+            }
+        }
+
+        return count;
+    }
+};
