d45529a08cca3b7a78ec549472655a15e47f19bc
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-01-30 12:27:30 +0530

    Create 30th_Jan - Evaluate Reverse Polish Notation.cpp

---
+++ DSA questions/POTD/Leetcode/30th_Jan - Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,28 @@
+// 150.Evaluate Reverse Polish Notation
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/description/?envType=daily-question&envId=2024-01-30
+
+class Solution {
+    int performOperation(int num1, int num2, char op) {
+        if (op == '+') return num1 + num2;
+        if (op == '-') return num1 - num2;
+        if (op == '*') return num1 * num2;
+        if (op == '/') return num1 / num2;
+        return 0;  // Handle unsupported operator
+    }
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for (auto& token : tokens) {
+            if (token.size() == 1 && !isdigit(token[0])) {  // Check if an operator
+                int num2 = s.top();
+                s.pop();
+                int num1 = s.top();
+                s.pop();
+                s.push(performOperation(num1, num2, token[0]));
+            } else {  // If a number, push to stack
+                s.push(stoi(token));
+            }
+        }
+        return s.top();
+    }
+};
