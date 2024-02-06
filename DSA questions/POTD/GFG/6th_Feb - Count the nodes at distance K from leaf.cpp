fe662f027b76225c3394fc363f2cea25acb3bac5
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-02-06 12:20:53 +0530

    Update 6th_Feb - Count the nodes at distance K from leaf.cpp

--- DSA questions/POTD/GFG/6th_Feb - Count the nodes at distance K from leaf.cpp
+++ DSA questions/POTD/GFG/6th_Feb - Count the nodes at distance K from leaf.cpp
@@ -1,3 +1,30 @@
 // Count the nodes at distance K from leaf
 // https://www.geeksforgeeks.org/problems/node-at-distance/1
 
+class Solution
+{
+    void helper(Node* root,int k,set<int> &s,int i,int n){
+        if(root==NULL)      return ;
+        
+        if( !root->left && !root->right && i==k)
+            s.insert(n);
+        
+        helper(root->left, k, s, i+1, n);
+        helper(root->right, k, s, i+1, n);
+        
+    }
+    public:
+    //Function to return count of nodes at a given distance from leaf nodes.
+    int printKDistantfromLeaf(Node* root, int k)
+    {
+        set<int> s;     
+        if(!root)   return s.size();
+        
+        helper(root, k, s, 0, root->data);
+        
+        int leftResult = printKDistantfromLeaf(root->left, k);
+        int rightResult = printKDistantfromLeaf(root->right, k);
+        
+        return s.size() + leftResult + rightResult;
+    }
+};
