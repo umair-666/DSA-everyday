59941dd017b538f7ca0cb8759b0295d609421314
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-02-10 13:31:55 +0530

    Create 10th_Feb - Palindromic Substrings.cpp

---
+++ DSA questions/POTD/Leetcode/10th_Feb - Palindromic Substrings.cpp
@@ -0,0 +1,25 @@
+// 647. Palindromic Substrings
+// https://leetcode.com/problems/palindromic-substrings/description/?envType=daily-question&envId=2024-02-10
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int even = palindromeCount(s, i, i + 1);
+            int odd = palindromeCount(s, i, i);
+            ans += even + odd;
+        }
+        return ans;
+    }
+
+    int palindromeCount(const string& s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            --left;
+            ++right;
+            ++count;
+        }
+        return count;
+    }
+};
