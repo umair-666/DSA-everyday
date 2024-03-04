5b995f030f4f1c49cb96844fd4122ff1acf3fb21
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-03-04 11:02:01 +0530

    4th_March - Swap the array elements.cpp

---
+++ DSA questions/POTD/GFG/4th_March - Swap the array elements.cpp
@@ -0,0 +1,13 @@
+// Swap the array elements
+// https://www.geeksforgeeks.org/problems/need-some-change/1
+
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        for(int i=0;i<n-2;i++){
+            swap(arr[i] , arr[i+2]);
+        }
+        
+    }
+};
