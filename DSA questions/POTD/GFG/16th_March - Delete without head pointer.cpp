c50818c1ad2b0e5a87609add4c518726612b5c7d
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-03-16 12:17:04 +0530

    Create 16th_March - Delete without head pointer.cpp

---
+++ DSA questions/POTD/GFG/16th_March - Delete without head pointer.cpp
@@ -0,0 +1,13 @@
+// Delete without head pointer
+// https://www.geeksforgeeks.org/problems/delete-without-head-pointer/1
+
+class Solution{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del){
+       // Your code here
+       Node* temp = del->next;
+       del->data = temp->data;
+       del->next = temp->next;
+       delete temp;
+    }};
