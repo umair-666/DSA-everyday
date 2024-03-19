6d177dfb5032e3448b5d06650108cca363bdf61f
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-03-19 21:40:35 +0530

    Create 19th_March - Task Scheduler.cpp

---
+++ DSA questions/POTD/Leetcode/19th_March - Task Scheduler.cpp
@@ -0,0 +1,22 @@
+// 621. Task Scheduler
+// https://leetcode.com/problems/task-scheduler/description/?envType=daily-question&envId=2024-03-19
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26);
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        sort(freq.begin(), freq.end());
+        int maxFreq = freq[25] - 1;
+        int idleSlots = maxFreq * n;
+
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+            idleSlots -= min(maxFreq, freq[i]);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+    }
+};
