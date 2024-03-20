77e792d48debd47ab77555d52282575503ed4915
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-03-20 12:28:09 +0530

    Create 20th_March - Merge In Between Linked Lists.cpp

---
+++ DSA questions/POTD/Leetcode/20th_March - Merge In Between Linked Lists.cpp
@@ -0,0 +1,31 @@
+// 1669. Merge In Between Linked Lists
+// https://leetcode.com/problems/merge-in-between-linked-lists/description/?envType=daily-question&envId=2024-03-20
+
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* start = nullptr;
+        ListNode* end = list1;
+
+        // Set start to node a - 1 and end to node b
+        for (int index = 0; index < b; index++) {
+            if (index == a - 1) {
+                start = end;
+            }
+            end = end->next;
+        }
+        // Connect the start node to list2
+        start->next = list2;
+
+        // Find the tail of list2
+        ListNode* tail = list2;
+        while (tail->next != nullptr) {
+            tail = tail->next;
+        }
+        // Set the tail of list2 to end.next
+        tail->next = end->next;
+        end->next =  nullptr;
+
+        return list1;
+    }
+};
