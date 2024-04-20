c01912aa13ff9801fd61e92aaf16763d02c1928a
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-04-20 15:51:41 +0530

    Create 20th_April - Union of Two Sorted Arrays.cpp

---
+++ DSA questions/POTD/GFG/20th_April - Union of Two Sorted Arrays.cpp
@@ -0,0 +1,25 @@
+// Union of Two Sorted Arrays
+// https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        set<int> s;
+        
+         for(int i=0;i<n;i++){
+            s.insert(arr1[i]);
+        }
+        
+        for(int i=0;i<m;i++){
+            s.insert(arr2[i]);
+        }
+        
+        vector<int> v(s.begin(), s.end());
+        return v;
+        
+    }
+};
