8d1e7f9c67013d16fd9f23b60882890c9f31324c
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-04-25 15:45:36 +0530

    Create 25th_April - Longest Ideal Subsequence.cpp

---
+++ DSA questions/POTD/Leetcode/25th_April - Longest Ideal Subsequence.cpp
@@ -0,0 +1,43 @@
+// 2370. Longest Ideal Subsequence
+// https://leetcode.com/problems/longest-ideal-subsequence/description/?envType=daily-question&envId=2024-04-25
+
+class Solution {
+    int dfs(int i, int c, vector<vector<int>>& dp, string& s, int k) {
+        // Memoized value
+        if (dp[i][c] != -1) {
+            return dp[i][c];
+        }
+
+        // State is not visited yet
+        dp[i][c] = 0;
+        bool match = c == (s[i] - 'a');
+        if (match) {
+            dp[i][c] = 1;
+        }
+        
+        // Non base case handling
+        if (i > 0) {
+            dp[i][c] = dfs(i - 1, c, dp, s, k);
+            if (match) {
+                for (int p = 0; p < 26; p++) {
+                    if (abs(c - p) <= k) {
+                        dp[i][c] = max(dp[i][c], dfs(i - 1, p, dp, s, k) + 1);
+                    }
+                }
+            }
+        }
+        return dp[i][c];
+    }
+public:
+    int longestIdealString(string s, int k) {
+        int N = size(s);
+        vector<vector<int>> dp(N, vector<int>(26, -1));
+
+        // Find the maximum dp[N-1][c] and return the result
+        int res = 0;
+        for (int c = 0; c < 26; c++) {
+            res = max(res, dfs(N - 1, c, dp, s, k));
+        }
+        return res;
+    }
+};
