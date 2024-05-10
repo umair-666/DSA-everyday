e6d395fefa38ebf73fa7dd8dfa87b07bfad3bd75
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-05-10 23:30:05 +0530

    Create 10th_May - K-th Smallest Prime Fraction.cpp

---
+++ DSA questions/POTD/Leetcode/10th_May - K-th Smallest Prime Fraction.cpp
@@ -0,0 +1,24 @@
+// 786. K-th Smallest Prime Fraction
+// https://leetcode.com/problems/k-th-smallest-prime-fraction/description/?envType=daily-question&envId=2024-05-10
+
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        
+        priority_queue<pair<double, pair<int, int>>> pq;
+
+        for (int i = 0; i < arr.size(); i++)
+            pq.push({-1.0 * arr[i] / arr.back(), {i, arr.size() - 1}});
+        
+        while (--k > 0) {
+            pair<int, int> cur = pq.top().second;
+            pq.pop();
+            cur.second--;
+            pq.push({-1.0 * arr[cur.first] / arr[cur.second], 
+                    {cur.first, cur.second}});
+        }
+        
+        pair<int, int> result = pq.top().second;
+        return {arr[result.first], arr[result.second]};
+    }
+};
