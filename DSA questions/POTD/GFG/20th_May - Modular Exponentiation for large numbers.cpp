f5527f9a273ff3aab1cf0795fa7773791261bda3
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-05-20 12:58:30 +0530

    Create 20th_May - Modular Exponentiation for large numbers.cpp

---
+++ DSA questions/POTD/GFG/20th_May - Modular Exponentiation for large numbers.cpp
@@ -0,0 +1,24 @@
+// Modular Exponentiation for large numbers
+// https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
+
+class Solution
+{
+	public:
+		long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    
+		   long long int ans=1;
+           
+           while(n>0){
+               if(n%2==0){
+                   x=(x*x)%M;
+                   n/=2;
+               }
+               else{
+                   ans=(ans*x)%M;
+                   n=n-1;
+               }
+           }
+           return ans%M;
+		}
+};
