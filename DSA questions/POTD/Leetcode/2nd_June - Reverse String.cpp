424354a3ca177a11fa15bb4c881f1469a360c4e7
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-06-02 23:48:47 +0530

    Create 2nd_June - Reverse String.cpp

---
+++ DSA questions/POTD/Leetcode/2nd_June - Reverse String.cpp
@@ -0,0 +1,9 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/description/?envType=daily-question&envId=2024-06-02
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(), s.end());
+    }
+};
