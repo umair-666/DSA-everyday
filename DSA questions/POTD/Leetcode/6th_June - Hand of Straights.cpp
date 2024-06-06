f2b29c6efe79225258d9ab1650c148a6c328facd
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-06-06 23:25:22 +0530

    Create 6th_June - Hand of Straights.cpp

---
+++ DSA questions/POTD/Leetcode/6th_June - Hand of Straights.cpp
@@ -0,0 +1,33 @@
+// 846. Hand of Straights
+// https://leetcode.com/problems/hand-of-straights/description/?envType=daily-question&envId=2024-06-06
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int handSize = hand.size();
+
+        if (handSize % groupSize != 0) {
+            return false;
+        }
+
+        map<int, int> cardCount;
+        for (int i = 0; i < handSize; i++) {
+            cardCount[hand[i]]++;
+        }
+
+        while (!cardCount.empty()) {
+            int currentCard = cardCount.begin()->first;
+            for (int i = 0; i < groupSize; i++) {
+                if (cardCount[currentCard + i] == 0) {
+                    return false;
+                }
+                cardCount[currentCard + i]--;
+                if (cardCount[currentCard + i] < 1) {
+                    cardCount.erase(currentCard + i);
+                }
+            }
+        }
+
+        return true;
+    }
+};
