2fceb9af6ba855ed536a9285de656d83494c1713
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-06-08 23:42:02 +0530

    Create 8th_June - Index of an Extra Element.cpp

---
+++ DSA questions/POTD/GFG/8th_June - Index of an Extra Element.cpp
@@ -0,0 +1,13 @@
+// Index of an Extra Element
+// https://www.geeksforgeeks.org/problems/index-of-an-extra-element/1
+
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        for(int i=0;i<n;i++){
+            if(arr1[i]!= arr2[i])
+                return i;
+        }
+    }
+};
+
