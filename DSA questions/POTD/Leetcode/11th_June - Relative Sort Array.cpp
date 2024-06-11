bbc771c47789166fbebab2550eb95ff2d8641df0
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-06-11 23:43:28 +0530

    Create 11th_June - Relative Sort Array.cpp

---
+++ DSA questions/POTD/Leetcode/11th_June - Relative Sort Array.cpp
@@ -0,0 +1,32 @@
+// 1122. Relative Sort Array
+// https://leetcode.com/problems/relative-sort-array/submissions/1285192485/?envType=daily-question&envId=2024-06-11
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        int maxElement = *max_element(arr1.begin(), arr1.end());
+        vector<int> count(maxElement + 1);
+
+        // Count occurrences of each element
+        for (int element : arr1) {
+            count[element]++;
+        }
+
+        vector<int> result;
+        // Add elements as per relative order
+        for (int element : arr2) {
+            while (count[element] > 0) {
+                result.push_back(element);
+                count[element]--;
+            }
+        }
+        // Add remaining elements in ascending order
+        for (int num = 0; num <= maxElement; num++) {
+            while (count[num] > 0) {
+                result.push_back(num);
+                count[num]--;
+            }
+        }
+        return result;
+    }
+};
