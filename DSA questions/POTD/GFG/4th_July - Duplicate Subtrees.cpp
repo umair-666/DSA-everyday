2657249ebb713fc6b3c594453301634d306e767e
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-07-04 23:32:32 +0530

    Create 4th_July - Duplicate Subtrees.cpp

---
+++ DSA questions/POTD/GFG/4th_July - Duplicate Subtrees.cpp
@@ -0,0 +1,23 @@
+// Duplicate Subtrees
+// https://www.geeksforgeeks.org/problems/duplicate-subtrees/1
+
+class Solution {
+    
+    unordered_map<string,int> mp;
+    vector<Node*> res;
+    
+    string DFS(Node*root){
+        if(!root) return "N";
+        
+        string str = DFS(root->left)+""+DFS(root->right)+to_string(root->data);
+        mp[str]++;
+        if(mp[str] == 2) res.push_back(root);
+        
+        return str;
+    }
+  public:
+    vector<Node*> printAllDups(Node* root) {
+        DFS(root);
+        return res;
+    }
+};
