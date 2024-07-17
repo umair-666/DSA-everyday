0371f9c6f5cca1260c04298a2693384c5c2b0b9b
Umair Usman <139118512+umair-666@users.noreply.github.com>
2024-07-17 23:43:07 +0530

    Create 17th_July - Construct Binary Tree from Parent Array.cpp

---
+++ DSA questions/POTD/GFG/17th_July - Construct Binary Tree from Parent Array.cpp
@@ -0,0 +1,48 @@
+// Construct Binary Tree from Parent Array
+// https://www.geeksforgeeks.org/problems/construct-binary-tree-from-parent-array/1
+
+
+
+/* node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to construct binary tree from parent array.
+    Node *createTree(vector<int> parent) {
+         vector<Node*>node;
+        int n=parent.size();
+        for(int i=0; i<n; i++){
+            node.push_back(new Node(i));
+        }
+        
+        Node* root=NULL;
+        
+        for(int i=0; i<n; i++){
+            if(parent[i] == -1){
+                root = node[i];
+            }
+            else{
+                if(!node[parent[i]]->left){
+                    node[parent[i]]->left = node[i];
+                }
+                else{
+                    node[parent[i]]->right = node[i];
+                }
+            }
+        }
+        
+        return root;
+    }
+};
